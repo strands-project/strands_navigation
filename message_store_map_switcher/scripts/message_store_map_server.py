b551c2dee4b45f5c302f49c7f28884589840f64b
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2014-02-24 22:14:08 +0000

    Added switching service with return for success/fail. Tested and working on two maps in rviz.

--- message_store_map_switcher/scripts/message_store_map_server.py
+++ message_store_map_switcher/scripts/message_store_map_server.py
@@ -8,19 +8,29 @@ from ros_datacentre.message_store import MessageStoreProxy
 import StringIO
 from optparse import OptionParser
 from nav_msgs.msg import OccupancyGrid
+from message_store_map_switcher.srv import SwitchMap
 
 class MessageStoreMapServer:
     def __init__(self):
         self.msg_store = MessageStoreProxy()
         self.map_publisher = rospy.Publisher('/map', OccupancyGrid, latch=True)
+        self.publish_map_serv = rospy.Service('/publish_map', SwitchMap, self.serve_map_srv)
+
+    def serve_map_srv(self, req):
+        return self.serve_map(req.map_name)         
 
     def serve_map(self, name):
         try:
             map = self.msg_store.query_named(name, OccupancyGrid._type)
-            self.map_publisher.publish(map)
+            if map is None:
+                rospy.logwarn("No map found with name \"%s\"", name)
+                return False
+            else:
+                self.map_publisher.publish(map)
+                return True
         except rospy.ServiceException, e:
             print "Service call failed: %s"%e
-
+            return False
 
 if __name__ == '__main__':
     rospy.init_node("message_store_map_server")
@@ -30,10 +40,13 @@ if __name__ == '__main__':
     (options, args) = parser.parse_args()
     map_name = rospy.get_param("default_map", options.map_name)
 
-    if map_name is None :
-        parser.error("Default map name must be set by either -d/--default or via the default_map ros param")
-
 
     map_server = MessageStoreMapServer()
-    map_server.serve_map(map_name)
+
+    if map_name is None :
+        rospy.logwarn("No default map set via -d/--default command line, or ros param \"default_map\", so no map being published")
+    else:
+        # serve the default map
+        map_server.serve_map(map_name)
+    
     rospy.spin()
