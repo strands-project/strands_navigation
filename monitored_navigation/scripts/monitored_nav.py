df273b453b97850a3874474655d1d22130ac9c16
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2014-10-29 17:06:39 +0000

    monitor and recovery states are now defined via a config yaml file.

--- monitored_navigation/scripts/monitored_nav.py
+++ monitored_navigation/scripts/monitored_nav.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import sys
+import yaml
 import rospy
 
 from dynamic_reconfigure.server import Server
@@ -12,36 +13,26 @@ from move_base_msgs.msg import MoveBaseAction
 from  monitored_navigation.navigation import HighLevelNav
 from strands_navigation_msgs.msg import MonitoredNavigationAction, MonitoredNavigationActionGoal
 
-#from strands_recovery_behaviours.recover_bumper_no_help import RecoverBumperNoHelp
-#from strands_recovery_behaviours.recover_bumper import RecoverBumper
-#from strands_recovery_behaviours.monitor_bumper import MonitorBumper
-#from strands_recovery_behaviours.monitor_stuck_on_carpet import MonitorStuckOnCarpet
-#from strands_recovery_behaviours.recover_stuck_on_carpet_no_help import RecoverStuckOnCarpetNoHelp
-#from strands_recovery_behaviours.recover_nav_no_help import RecoverNavNoHelp 
-#from strands_recovery_behaviours.recover_nav import RecoverNav
    
 class MonitoredNavigation(ActionServerWrapper):
 
-    def __init__(self):
+    def __init__(self, filename):
         
         # Create the main state machine
         self.high_level_nav = HighLevelNav()
         
         
-        #TODO: initialize recovery behaviours via yaml file. Add services for changing recoveries at runtime
-        #recover = RecoverBumper()
-        #monitor=MonitorBumper()
+        ##TODO:  Add services for changing recoveries at runtime
         
-        #self.high_level_nav.add_monitor_recovery_pair(monitor,recover,"bumper")
-        
-        #monitor=MonitorStuckOnCarpet()
-        #recover=RecoverStuckOnCarpetNoHelp()
-        
-        #self.high_level_nav.add_monitor_recovery_pair(monitor,recover,"stuck_on_carpet")
-        
-        
-        #recover=RecoverNav()
-        #self.high_level_nav.set_nav_recovery(recover)
+        if file_name is not None:
+            stream = open(file_name, 'r')
+            yaml_config=yaml.load(stream)
+            nav_recovery_dict=yaml_config["nav_recovery"]
+            self.high_level_nav.set_nav_recovery(self.create_recovery_object(nav_recovery_dict))
+
+            monitor_recovery_pairs=yaml_config["monitor_recovery_pairs"]
+            for monitor_recovery_pair in monitor_recovery_pairs:
+                self.high_level_nav.add_monitor_recovery_pair(self.create_monitor_object(monitor_recovery_pair),self.create_recovery_object(monitor_recovery_pair),monitor_recovery_pair["name"])
         
         self.sis = IntrospectionServer('monitored_navigation_sm', self.high_level_nav.high_level_sm, '/MON_NAV')
         self.sis.start()
@@ -54,7 +45,17 @@ class MonitoredNavigation(ActionServerWrapper):
     
 
     
-    
+    def create_recovery_object(self, recovery_dict):
+        mod = __import__(recovery_dict["package"]+ '.' + recovery_dict["recovery_file"], fromlist=[recovery_dict["recovery_class"]])
+        recovery_class=getattr(mod, recovery_dict["recovery_class"])
+        return recovery_class()
+        
+    def create_monitor_object(self, monitor_dict):
+        mod = __import__(monitor_dict["package"]+ '.' + monitor_dict["monitor_file"], fromlist=[monitor_dict["monitor_class"]])
+        monitor_class=getattr(mod, monitor_dict["monitor_class"])
+        return monitor_class()
+        
+        
     
     def main(self):
         
@@ -67,6 +68,13 @@ class MonitoredNavigation(ActionServerWrapper):
 
 if __name__ == '__main__':
     rospy.init_node('monitored_navigation')
- 
-    mon_nav =  MonitoredNavigation()
+    
+    if len(sys.argv) < 1 or sys.argv[1]=="none":
+        rospy.logwarn("No config yaml file provided. MonitoredNavigation state machine will be initialized without recovery behaviours. The strands config yaml file is located in monitored_navigation/config/strands.yaml")
+        file_name=None
+    else:
+        file_name=sys.argv[1]
+
+   
+    mon_nav =  MonitoredNavigation(file_name)
     mon_nav.main()
