4ada0d69f175a84acf4fc248bdc17a65466ab550
Rares Ambrus <raambrus@kth.se>
2015-02-11 09:42:05 +0000

    Clean up

--- topological_utils/scripts/node_metadata.py
+++ topological_utils/scripts/node_metadata.py
@@ -12,17 +12,6 @@ from mongodb_store.message_store import MessageStoreProxy
 from topological_utils.queries import get_nodes
 from topological_utils.srv import *
 
-from random import randint
-
-import uuid
-
-def my_random_string(string_length=10):
-    """Returns a random string of length string_length."""
-    random = str(uuid.uuid4()) # Convert UUID format to a Python string.
-    random = random.upper() # Make all characters uppercase.
-    random = random.replace("-","") # Remove the UUID '-'.
-    return random[0:string_length]
-
 def handle_query_nodes(req):
     print "Queried for %s %s %s"%(req.map_name, req.pointset, req.meta_category)
     
@@ -50,14 +39,11 @@ def handle_query_nodes(req):
 
 def query_nodes_server():
     rospy.init_node('query_nodes_server')
-    s = rospy.Service('query_nodes', NodeMetadata, handle_query_nodes)
+    s = rospy.Service('query_node_metadata', NodeMetadata, handle_query_nodes)
     print "Ready to query the database for topological nodes."
     rospy.spin()
 
 if __name__ == '__main__':
-    #point_set=str(sys.argv[1])
-    
-    #get_nodes(point_set)
     query_nodes_server()
 
 
