dff502c8748753fb7b37d9f98f396eaf2d12ae49
Jaime Pulido Fentanes <pulidofentanes@gmail.com>
2015-02-18 16:33:02 +0000

    Adding recovery behaviours to edges

--- topological_utils/scripts/topological_map_update.py
+++ topological_utils/scripts/topological_map_update.py
@@ -3,6 +3,7 @@ import math
 import rospy
 import sys
 
+from time import sleep
 import std_msgs.msg
 
 
@@ -10,6 +11,10 @@ if __name__ == '__main__' :
     rospy.init_node("topological_map_update")
     rospy.loginfo("Publishing update request for topological map ...")
     map_update = rospy.Publisher('/update_map', std_msgs.msg.Time)
-    map_update.publish(rospy.Time.now())
+
+    rospy.sleep(rospy.Duration.from_sec(1))
+    tmstp = std_msgs.msg.Time()
+    tmstp.data = rospy.Time.now()
+    map_update.publish(tmstp)
     rospy.loginfo("All Done ...")
     rospy.loginfo("Bye")
