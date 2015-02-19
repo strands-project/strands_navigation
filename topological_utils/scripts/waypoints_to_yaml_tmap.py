06c744565a2bd0cbfb850488032990b57a64acf4
Jailander <pulidofentanes@gmail.com>
2015-02-19 03:09:31 +0000

    New map format export and insertion scripts

--- topological_utils/scripts/waypoints_to_yaml_tmap.py
+++ topological_utils/scripts/waypoints_to_yaml_tmap.py
@@ -50,18 +50,19 @@ def get_empty_edge(mapname,standard_action):
     return e
 
 if __name__ == '__main__':
-    if len(sys.argv) < 4 :
-        print "usage: tmap_from_waypoint input_file pointset map [max_dist_connect]"
+    if len(sys.argv) < 5 :
+        print "usage: tmap_from_waypoint input_file output_file pointset map [max_dist_connect]"
         sys.exit(2)
         
     filename=str(sys.argv[1])
-    pointset=str(sys.argv[2])
-    mapname=str(sys.argv[3])
+    outfile=str(sys.argv[2])
+    pointset=str(sys.argv[3])
+    mapname=str(sys.argv[4])
 
     vertices=[(0.69, 0.287), (0.287, 0.69), (-0.287, 0.69), (-0.69, 0.287), (-0.69, -0.287), (-0.287, -0.69), (0.287, -0.69), (0.69, -0.287)]
 
-    if len(sys.argv) == 5:
-        max_dist_connect=float(sys.argv[4])
+    if len(sys.argv) == 6:
+        max_dist_connect=float(sys.argv[5])
     else:
         max_dist_connect=100
     
@@ -85,6 +86,8 @@ if __name__ == '__main__':
         n.yaw_goal_tolerance = 0.1
         n.xy_goal_tolerance = 0.3
         n.verts = get_vertex_list(vertices)
+        #n = dc_util.msg_to_document(n1)
+        #print n
         m = {}
         m["map"] = mapname
         m["pointset"] = pointset
@@ -96,10 +99,12 @@ if __name__ == '__main__':
     fin.close()
 
 #    for i in lnodes:        print i["node"]
-
+    print "calculate edges"
     edge_names = []
     for i in lnodes:
+        #im = dc_util.dictionary_to_message(i[0], TopologicalNode)
         for j in lnodes:
+            #jm = dc_util.dictionary_to_message(j[0], TopologicalNode)
             if node_dist(i[0],j[0]) < max_dist_connect and i[0].name != j[0].name :
                 print "connecting %s to %s"%(i[0].name,j[0].name)
                 e = get_empty_edge(mapname, standard_action)
@@ -107,98 +112,23 @@ if __name__ == '__main__':
                 e.node = j[0].name
                 i[0].edges.append(e)
 
+    onodes = []
+    for i in lnodes:
+        r = []
+        q = dc_util.msg_to_document(i[0])
+        r.append(q)
+        r.append(i[1])
+        onodes.append(r)
 
-    for i in lnodes:                   
-        yml = yaml.dump(i[1])
-        print yml
+
+    yml = yaml.safe_dump(onodes, default_flow_style=False)
+    print yml
     #print s_output
 
-
-
-
-
-
-    #max speed 0.55
-
-
-
-#    #Inserting charging point
-#    nnodes=0
-#    #line = fin.readline()
-#    line = "0,0,0,0,0,0,0\n"
-#    cnode=Topo_node("ChargingPoint",line)
-#    #lnodes=[node]
-#    
-#    lnodes=[]
-#    line = fin.readline()
-#    #Inserting waypoints
-#    while line:
-#        nnodes=nnodes+1
-#        wname= "WayPoint%d" %nnodes
-#        node=Topo_node(wname,line)
-#        lnodes.append(node)
-#        line = fin.readline()
-#    fin.close()
-#    
-#    #inserting edges
-#    cedges=[]
-#    cedge = {'node':lnodes[0].node_name, 'action':"undocking"}
-#    cedges.append(cedge)
-#    cnode._insert_edges(cedges)
-#    
-#    nnodes=len(lnodes)
-#    eind=0
-#    for i in lnodes :
-#        edge = {'node':"empty", 'action':"move_base"}
-#        edges=[edge]
-#        if eind == 0:
-#            edge = {'node':cnode.node_name, 'action':"docking"}
-#            edges.append(edge)
-#        for j in lnodes :
-#            if i.node_name is not j.node_name and node_dist(i.waypoint,j.waypoint)<max_dist_connect:
-#                node_dist(i.waypoint,j.waypoint)
-#                edge = {'node':j.node_name, 'action':"human_aware_navigation"}
-#                edges.append(edge)
-#                i._insert_edges(edges)
-#        eind+=1
-#        i.edges.pop(0)
-#
-#
-#    #inserting corners
-#    cvertices=[(0.25, 0.69), (-0.287, 0.69), (-0.69, 0.287), (-0.69, -0.287), (-0.287, -0.69), (0.25, -0.69)]
-#    cnode._insert_vertices(cvertices)
-#
-#    for i in lnodes :
-#        vertices=[(0.69, 0.287), (0.287, 0.69), (-0.287, 0.69), (-0.69, 0.287), (-0.69, -0.287), (-0.287, -0.69), (0.287, -0.69), (0.69, -0.287)]
-#        i._insert_vertices(vertices)
-#
-#    lnodes.insert(0,cnode)
-#    
-#    #Clean the file in case it existed
-#    fh = open(outfile, "w")
-#    fh.close
-#
-#    #Write File
-#    for i in lnodes :
-#        fh = open(outfile, "a")
-#        print "node: \n\t%s" %i.node_name
-#        s_output = "node: \n\t%s\n" %i.node_name
-#        fh.write(s_output)
-#        print "\twaypoint:\n\t%s" %i.waypoint
-#        s_output = "\twaypoint:\n\t\t%s" %i.waypoint
-#        fh.write(s_output)
-#        print "\tedges:"
-#        s_output = "\tedges:\n"
-#        fh.write(s_output)
-#        for k in i.edges :
-#            print "\t\t %s, %s" %(k['node'],k['action'])
-#            s_output = "\t\t %s, %s\n" %(k['node'],k['action'])
-#            fh.write(s_output)
-#        print "\tvertices:"
-#        s_output = "\tvertices:\n"
-#        fh.write(s_output)
-#        for k in i.vertices :
-#            print "\t\t%f,%f" %(k[0],k[1])
-#            s_output = "\t\t%f,%f\n" %(k[0],k[1])
-#            fh.write(s_output)
-#        fh.close        
+    fh = open(outfile, "w")
+    #s_output = str(bson.json_util.dumps(nodeinf, indent=1))
+    s_output = str(yml)
+    print s_output
+    fh.write(s_output)
+    fh.close
+       
