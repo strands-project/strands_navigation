814d87b34975e5653e80eac50fef76dfb73997ed
Christian Dondrup <cdondrup@lincoln.ac.uk>
2015-03-24 00:30:46 +0000

    Finishing basic functionality. Node subscribes to current edge and topic and checks against the white list if it is in it. If so, publish true, false in all other cases.
    Adding a whitelist that allows all nodes. Triggered by keyword `ALL`. Therefore no node can ever be named `ALL`.

--- topological_logging_manager/scripts/logging_manager.py
+++ topological_logging_manager/scripts/logging_manager.py
@@ -7,6 +7,7 @@ Created on Mon Mar 23 12:08:14 2015
 """
 
 import rospy
+import rosparam
 from std_msgs.msg import String, Bool
 from topological_logging_manager.msg import LoggingManager
 import thread
@@ -16,9 +17,17 @@ class Manager(object):
 
     def __init__(self, name):
         rospy.loginfo("Starting node: %s" % name)
-        self.current_edge = None
-        self.current_node = None
-        self.white_list = rospy.get_param("~white_list", None)
+        self.current_edge = 'none'
+        self.current_node = 'none'
+        self.alive = False
+
+        white_list_file = rospy.get_param("~white_list_file", '')
+        if white_list_file == '':
+            rospy.logfatal("No white list for logging specified")
+            raise rospy.ROSException("No white list for logging specified")
+            return
+        self.white_list = rosparam.load_file(white_list_file)[0][0]
+
         self.node_sub = rospy.Subscriber(
             rospy.get_param("~node_topic", "/current_node"),
             String,
@@ -41,14 +50,24 @@ class Manager(object):
             queue_size=10
         )
 
-        self.rate = rospy.Rate(rospy.get_param("~publishing_rate", 30))
-        thread.start_new_thread(self.publisher_thread,(self.rate,))
+        heart_beat = rospy.Rate(rospy.get_param("~check_topic_rate", 1))
+        thread.start_new_thread(self.check_topics,(self.node_sub, self.edge_sub, heart_beat))
+
+        pub_rate = rospy.Rate(rospy.get_param("~publishing_rate", 30))
+        thread.start_new_thread(self.publisher_thread,(pub_rate,))
 
     def node_callback(self, msg):
-        self.current_node = msg.data if not msg.data == "none" else None
+        self.current_node = msg.data
 
     def edge_callback(self, msg):
-        self.current_edge = msg.data if not msg.data == "none" else None
+        self.current_edge = msg.data
+
+    def check_topics(self, sub1, sub2, rate):
+        while not rospy.is_shutdown():
+            self.alive = True if sub1.get_num_connections() and sub2.get_num_connections() else False
+            if not self.alive:
+                rospy.logwarn("Subscribed topics are not being published.")
+            rate.sleep()
 
     def publisher_thread(self, rate):
         if not self.white_list:
@@ -59,14 +78,20 @@ class Manager(object):
         while not rospy.is_shutdown():
             res = LoggingManager()
             res.header.stamp = rospy.Time.now()
-            if self.current_edge or self.current_node:
-                self.pub_bool.publish(True)
-                res.log = True
-                self.pub_bool_stamped.publish(res)
+            res.log = False
+            if self.alive:
+                if self.current_edge or self.current_node:
+                    if type(self.white_list) == dict:
+                        if [x for x in self.white_list["nodes"] if self.current_node in x or "ALL" in x] \
+                            or [x for x in self.white_list["edges"] if self.current_edge in x or "ALL" in x]:
+                                res.log = True
             else:
-                self.pub_bool.publish(False)
-                res.log = False
-                self.pub_bool_stamped.publish(res)
+                if not self.current_edge == "none" or self.current_node == "none":
+                    self.current_edge = "none"
+                    self.current_node = "none"
+
+            self.pub_bool.publish(res.log)
+            self.pub_bool_stamped.publish(res)
             rate.sleep()
 
 if __name__ == "__main__":
