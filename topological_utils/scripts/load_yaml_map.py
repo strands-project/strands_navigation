1afa2eb370e7bca1bcc21b7deda753aa587a053e
Christian Dondrup <cdondrup@lincoln.ac.uk>
2015-11-05 13:54:51 +0000

    Extending the load yaml map functionality. Now based on a class in topological navigation to prevent circular test dependencies.

--- topological_utils/scripts/load_yaml_map.py
+++ topological_utils/scripts/load_yaml_map.py
@@ -1,57 +1,32 @@
 #!/usr/bin/env python
 
+import rospy
 import yaml
-import sys
+import argparse
 
-from strands_navigation_msgs.msg import TopologicalNode
-import mongodb_store.util as dc_util
-from mongodb_store.message_store import MessageStoreProxy
+from topological_navigation.load_maps_from_yaml import YamlMapLoader
+from std_srvs.srv import Empty
 
+
+def load_yaml(filename):
+    rospy.loginfo("loading %s"%filename)
+    with open(filename, 'r') as f:
+        return yaml.load(f)
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2 :
-        print "usage: insert_map input_file.txt"
-	sys.exit(2)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("yaml_map", help="The yaml file or directory containing yaml files to insert into the datacentre", type=str)
+    parser.add_argument("--pointset", help="Override the pointset name defined in the yaml before inserting it into datacentre, i.e. rename map. If a directory is given the pointset will be extended by an index to keep the maps separate.", type=str)
+    parser.add_argument("-f", "--force", help="Override maps with the same name in the datacentre", action="store_true")
+    parser.add_argument("--keep-alive", help="Keeps the node alive and advertises the service '/load_yaml_map/loaded' as soon as all the maps are stored in the datacentre. Can be used for testing in a launch file to trigger components that rely on maps being inserted in the datacentre first.", action="store_true")
+    args, unknown = parser.parse_known_args() # Necessary due to roslaunch injecting rubbish arguments
 
-    filename=str(sys.argv[1])
+    rospy.init_node('load_yaml_map')
+    y = YamlMapLoader()
 
-    msg_store = MessageStoreProxy(collection='topological_maps')
+    data = y.read_maps(args.yaml_map)
+    y.insert_maps(data=data, new_pointset=args.pointset, force=args.force)
 
-    print "loading %s"%filename
-    yaml_data=open(filename, "r")
-    
-    data = yaml.load(yaml_data)
-    
-    print "printing vlaa"
-
-    
-    for i in data:
-        meta = i['meta']
-        print i['node']
-        msgv = dc_util.dictionary_to_message(i['node'], TopologicalNode)
-        #print msgv, meta
-        msg_store.insert(msgv,meta)
-        #mongodb_store.util.store_message(points_db,p,val)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if args.keep_alive:
+        rospy.Service("~loaded", Empty, None)
+        rospy.spin()
