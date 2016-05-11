32b204e6bc84bf45fde132638f67984b7e1aa572
Bruno Lacerda <b.lacerda@cs.bham.ac.uk>
2016-05-11 17:15:29 +0100

    allowing for userdata between monitor and recover states

--- monitored_navigation/src/monitored_navigation/monitor_state.py
+++ monitored_navigation/src/monitored_navigation/monitor_state.py
@@ -7,5 +7,5 @@ import smach_ros
 
 
 class MonitorState(smach_ros.MonitorState):
-    def __init__(self,topic, msg_type, cb):
-        smach_ros.MonitorState.__init__(self, topic, msg_type, cb)
+    def __init__(self,topic, msg_type, cb, input_keys=[], output_keys=[]):
+        smach_ros.MonitorState.__init__(self, topic, msg_type, cb, -1, input_keys, output_keys)
