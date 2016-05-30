bb6994bbbe2f0ba83c7eee2486ad225c99cdb280
Nick Hawes <n.a.hawes@cs.bham.ac.uk>
2016-05-30 23:49:30 +0100

    Added simple node to report manually provided edge predictions from a yaml file.

---
+++ topological_navigation/scripts/manual_edge_predictions.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+import actionlib
+import pymongo
+import json
+import sys
+import math
+import time
+import yaml
+
+from datetime import datetime
+
+import actionlib
+from actionlib_msgs.msg import *
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Header
+from std_msgs.msg import String
+from nav_msgs.srv import *
+
+
+import strands_navigation_msgs.msg
+from strands_navigation_msgs.msg import TopologicalNode
+from mongodb_store.message_store import MessageStoreProxy
+from strands_navigation_msgs.msg import NavStatistics
+from strands_navigation_msgs.msg import TopologicalMap
+
+from topological_navigation.tmap_utils import *
+
+from strands_navigation_msgs.srv import *
+from strands_navigation_msgs.msg import *
+
+
+def usage():
+    print "Run with a yaml file containing edge prediction information:"
+    print "\t rosrun topological_navigation manual_edge_predictions.py <yaml file>"
+
+
+class YamlConfiguredPredictor(object):
+
+    def __init__(self, config) :
+
+        self.response = PredictEdgeStateResponse()
+
+        for edge in config:            
+            self.response.edge_ids.append(edge['edge_id'])
+            self.response.probs.append(edge['success_probability'])
+            self.response.durations.append(rospy.Duration(edge['duration']))
+
+        name= rospy.get_name()
+        action_name = name+'/build_temporal_model'
+
+        #Creating Action Server
+        rospy.loginfo("Creating action server.")
+        self._as = actionlib.SimpleActionServer(action_name, BuildTopPredictionAction, execute_cb = self.build_callback, auto_start = False)
+        self._as.start()
+
+
+        self.predict_srv=rospy.Service('topological_prediction/predict_edges', PredictEdgeState, self.predict_edge_cb)
+        rospy.loginfo("All Done ...")
+        rospy.spin()
+
+
+    def predict_edge_cb(self, req):
+        return self.response
+
+
+    """
+     Build predictions from data. Or not.
+     
+     Does nothing for this version.
+    """
+    def build_callback(self, goal):    
+        self._as.set_succeeded()
+
+
+if __name__ == '__main__':
+    rospy.init_node('topological_prediction')
+
+    if len(sys.argv) != 2:
+        usage()
+        sys.exit(1)
+    config_file = sys.argv[1]
+
+    
+    with open(config_file, "r") as f:
+        config = yaml.load(f.read())
+
+    server = YamlConfiguredPredictor(config)
