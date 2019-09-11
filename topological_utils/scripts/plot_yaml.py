14068a1e58dc31cae3e29dce5fe59436df6b298b
gpdas <contact@gauthampdas.com>
2019-09-11 17:36:03 +0100

    new script to plot topomaps from yaml files
    1. plot_yaml.py
    2. minor modification to plot_topo_map.py

--- topological_utils/scripts/plot_yaml.py
+++ topological_utils/scripts/plot_yaml.py
@@ -10,40 +10,46 @@ import os
 import sys
 import rospy
 import matplotlib.pyplot
-
-import strands_navigation_msgs.msg
+import yaml
 
 
 class TopoMapPlotter(object):
+    """TopoMapPlotter class definition
     """
-    """
-    def __init__(self, fig_file, fig_width, fig_height):
+    def __init__(self, yaml_file, fig_file, fig_width, fig_height):
+        """initialise TopoMapPlotter object
+
+        Keyword arguments:
+
+        yaml_file -- topomap in yaml format
+        fig_file -- name of the map image file to be saved with extension
+        fig_width -- width of map image file
+        fig_height -- height of map image file
         """
-        """
-        self.topo_map = None
-        self.rec_map = False
-        self.nodes = {}
-        self.node_names = []
-        rospy.loginfo("Waiting for topo_map")
-        while not self.rec_map:
-            self.topo_map_sub = rospy.Subscriber("/topological_map", strands_navigation_msgs.msg.TopologicalMap, self.topo_map_cb)
-            rospy.sleep(0.5)
-        rospy.loginfo("Received topo_map")
 
         self.fig_file = fig_file
         self.width = fig_width
         self.height = fig_height
 
-    def topo_map_cb(self, msg):
+        f_handle = open(yaml_file, "r")
+        self.topo_map = yaml.load(f_handle)
+        f_handle.close()
+
+        self.nodes = {}
+        self.node_names = []
+        self._extract_topo_map()
+
+
+    def _extract_topo_map(self):
+        """extract node infor from topomap
         """
-        """
-        self.topo_map = msg
-        for node in self.topo_map.nodes:
-            self.nodes[node.name] = node
-            self.node_names.append(node.name)
-        self.rec_map = True
+        for node in self.topo_map:
+            self.nodes[node["node"]["name"]] = node["node"]
+            self.node_names.append(node["node"]["name"])
 
     def plot_map(self, strip_str=""):
+        """plot into the topo map image file
+        """
         fig = matplotlib.pyplot.figure(figsize=(self.width, self.height))
         min_x = None
         max_x = None
@@ -52,8 +58,8 @@ class TopoMapPlotter(object):
         ax = fig.add_subplot(111)
         # node markers
         for node_name in self.node_names:
-            x = self.nodes[node_name].pose.position.x
-            y = self.nodes[node_name].pose.position.y
+            x = self.nodes[node_name]["pose"]["position"]["x"]
+            y = self.nodes[node_name]["pose"]["position"]["y"]
             ax.plot(x, y, color="black", marker="o", markersize=6, markeredgecolor="r")
 
             if min_x is None:
@@ -75,17 +81,17 @@ class TopoMapPlotter(object):
         y_delta = (max_y - min_y) * 0.1
         # node names
         for node_name in self.node_names:
-            x = self.nodes[node_name].pose.position.x
-            y = self.nodes[node_name].pose.position.y
+            x = self.nodes[node_name]["pose"]["position"]["x"]
+            y = self.nodes[node_name]["pose"]["position"]["y"]
             ax.text(x-x_delta/4.0, y + y_delta/4.0, node_name.strip(strip_str), fontsize=10)
 
         for node_name in self.node_names:
-            from_x = self.nodes[node_name].pose.position.x
-            from_y = self.nodes[node_name].pose.position.y
-            for edge in self.nodes[node_name].edges:
-                to_node = edge.node
-                to_x = self.nodes[to_node].pose.position.x
-                to_y = self.nodes[to_node].pose.position.y
+            from_x = self.nodes[node_name]["pose"]["position"]["x"]
+            from_y = self.nodes[node_name]["pose"]["position"]["y"]
+            for edge in self.nodes[node_name]["edges"]:
+                to_node = edge["node"]
+                to_x = self.nodes[to_node]["pose"]["position"]["x"]
+                to_y = self.nodes[to_node]["pose"]["position"]["y"]
 #                ax.plot([from_x, to_x], [from_y, to_y], color="black", linewidth=2)
                 ax.arrow(from_x, from_y, to_x-from_x, to_y-from_y, color="black",
                          head_width=0.2 * min(x_delta, y_delta),
@@ -96,22 +102,21 @@ class TopoMapPlotter(object):
         ax.set_ylabel("Y")
         ax.set_xlim(min_x - x_delta, max_x + x_delta)
         ax.set_ylim(min_y - y_delta, max_y + y_delta)
-        matplotlib.pyplot.show()
         fig.savefig(self.fig_file)
 
 if __name__ == "__main__":
-    rospy.init_node("plot_topo_map")
+    rospy.init_node("plot_yaml")
 
-    if len(sys.argv) < 4:
-        rospy.loginfo("Usage is plot_topo_map path_to_plot_file fig_width_inches fig_height_inches")
+    if len(sys.argv) < 5:
+        rospy.loginfo("Usage is plot_yaml <path_to_yaml_topomap> <path_to_plot_file> <fig_width_inches> <fig_height_inches>")
     else:
-        fig_file = os.path.abspath(sys.argv[1])
+        yaml_map = os.path.abspath(sys.argv[1])
+        fig_file = os.path.abspath(sys.argv[2])
 
         try:
-            map_plotter = TopoMapPlotter(fig_file, float(sys.argv[2]), float(sys.argv[3]))
+            map_plotter = TopoMapPlotter(yaml_map, fig_file, float(sys.argv[3]), float(sys.argv[4]))
         except:
             rospy.signal_shutdown("Couldn't create TopoMapPlotter object")
         else:
             map_plotter.plot_map(strip_str="WayPoint")
 
-#    rospy.spin()
