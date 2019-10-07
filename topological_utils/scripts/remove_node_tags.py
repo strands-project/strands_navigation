68a7e676fee493911e205de3bf218379debcd3b2
Gautham P Das <contact@gauthampdas.com>
2019-10-07 19:57:50 +0100

    scripts to add/remove tags to yaml topomaps (#381)
    * scripts to add/remove tags to yaml topomaps
    
    usage:
      add_node_tags.py <in_yaml_topomap> out_yaml_topomap> <node-tag-config-yaml>
      remove_node_tags.py <in_yaml_topomap> out_yaml_topomap> <node-tag-config-yaml>
    
    takes a yaml formatted config file for node tags.
    example
    ```
    charging_node:
      - WayPoint10
    door_transitions:
      - WayPoint4
      - WayPoint5
      - WayPoint24
      - WayPoint25
    ```
    
    * bug fix and cleanup

---
+++ topological_utils/scripts/remove_node_tags.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 12 20:21:38 2019
+
+@author: gpdas
+
+"""
+
+
+import yaml
+import sys
+import os
+
+
+def load_data_from_yaml(file_path):
+    assert os.path.exists(file_path)
+
+    f_handle = open(file_path, "r")
+    f_data = yaml.load(f_handle)
+    f_handle.close()
+    return f_data
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print ("Usage: remove_node_tags.py <in_file> <out_file> <config_file>")
+    else:
+        in_file = sys.argv[1]
+        out_file = sys.argv[2]
+        cfg_file = sys.argv[3]
+
+        topo_map = load_data_from_yaml(in_file)
+
+        tags = load_data_from_yaml(cfg_file)
+        node_tag = {}
+        for tag in tags:
+            for node in tags[tag]:
+                node_tag[node] = tag
+
+        for node in topo_map:
+            if node["meta"]["node"] in node_tag:
+                if "tag" in node["meta"]:
+                    if node_tag[node["meta"]["node"]] in node["meta"]["tag"]:
+                        node["meta"]["tag"].remove(node_tag[node["meta"]["node"]])
+                        if not node["meta"]["tag"]:
+                            node["meta"].pop("tag")
+
+        with open(out_file, 'w') as f_out:
+            yaml.dump(topo_map, f_out, default_flow_style=False)
+        print("Successfully created the new file removing configured node tags")
